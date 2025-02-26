0d64e747c8cb2fbcb22890ebabfcaf092a9858be
bir-dv <brukjoseph@gmail.com>
2025-02-26 11:23:57 +0300

    Apply inline css

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+<main style={{backgroundColor: 'gray', color: 'white', textAlign: 'center'}}>
+  <p>I love to visit New York, Paris, and Tokyo.</p>
+</main>
+    );
+}
+
+export default MainContent;
