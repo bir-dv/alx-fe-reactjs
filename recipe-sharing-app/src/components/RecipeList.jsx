713a14d6dbfec036f85cd239d259369ee567599a
bir-dv <brukjoseph@gmail.com>
2025-03-02 23:54:09 +0300

    add Search and Filtering Capabilities

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,22 +1,25 @@
-//desplay the list of recipes
-import React from 'react'
-// import  useRecipeStore  from '../store/useRecipeStore';
+import React, { useEffect } from 'react';
 import useRecipeStore from './recipeStore';
 
 const RecipeList = () => {
-    const recipes = useRecipeStore(state => state.recipes) || [];
-  return (
-    <div>
-        {
-            recipes.map(recipe => (
+    const recipes = useRecipeStore(state => state.filteredRecipes) || [];
+    const filterRecipes = useRecipeStore(state => state.filterRecipes);
+    const searchTerm = useRecipeStore(state => state.searchTerm);
+
+    useEffect(() => {
+        filterRecipes();
+    }, [searchTerm, filterRecipes]);
+
+    return (
+        <div>
+            {recipes.map(recipe => (
                 <div key={recipe.id}>
                     <h2>{recipe.title}</h2>
                     <p>{recipe.description}</p>
                 </div>
-            ))
-        }
-    </div> 
+            ))}
+        </div>
     );
 };
 
-export default RecipeList
+export default RecipeList;
