4aacac14b59b25f317958c6deac055de1a7081a0
bir-dv <brukjoseph@gmail.com>
2025-03-02 23:59:44 +0300

    Implementing User Favorites and Personalized Recipe Recommendations

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -4,6 +4,8 @@ import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeDetails from "./components/RecipeDetails";
 import EditRecipeForm from "./components/EditRecipeForm";
+import FavoritesList from "./components/FavoritesList";
+import RecommendationsList from "./components/RecommendationsList";
 
 const App = () => {
   return (
@@ -12,6 +14,8 @@ const App = () => {
         <h1>Recipe Sharing App</h1>
         <AddRecipeForm />
         <RecipeList />
+        <FavoritesList />
+        <RecommendationsList />
         <Routes>
           <Route path="/recipe/:id" element={<RecipeDetails />} />
           <Route path="/edit-recipe/:id" element={<EditRecipeForm />} />
