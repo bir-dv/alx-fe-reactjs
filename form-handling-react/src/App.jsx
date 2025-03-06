8cf38afc324d6c247558eb6b6e7c67c2409614b2
bir-dv <brukjoseph@gmail.com>
2025-03-06 14:23:57 +0300

    Formik for Form Handling

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,9 +1,14 @@
 import RegistrationForm from "./components/RegistrationForm";
+import FormikForm from "./components/formikForm";
 
 const App = () => {
   return(
     <div>
       <RegistrationForm />
+      <br />
+
+      <h3>controll the form using formik</h3>
+      <FormikForm />
     </div>
   )
 }
