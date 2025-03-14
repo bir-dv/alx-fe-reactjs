4f58571042ed74fa2bcc86526fe05fef82e1d7d5
bir-dv <brukjoseph@gmail.com>
2025-03-14 20:41:13 +0300

    Setting up tailwind CSS

---
+++ tailwind-react-integration/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false,
+  variants: { extend: {} },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
