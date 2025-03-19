a795823bd26d6d66c1e5efa1d1cf00c78b6b99dd
bir-dv <brukjoseph@gmail.com>
2025-03-19 09:10:03 +0300

    Setting up with tailwind

---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    './public/index.html',
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
